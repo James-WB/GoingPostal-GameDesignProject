32587f85651d437db0194612930e1b23229275be
jbonvive@uwo.ca <Gl@sgo1122>
2020-03-29 13:16:07 -0400

    Adding Tutorial

--- GoingPostal/Assets/Scripts/DeathPit.cs
+++ GoingPostal/Assets/Scripts/DeathPit.cs
@@ -7,11 +7,14 @@ public class DeathPit : MonoBehaviour
 
     public Transform startPos;
     public GameObject gameO;
+    public GameObject vcam;
 
     public Animator animator;
 
     void OnTriggerEnter2D(Collider2D collider){
 
         gameO.transform.position = startPos.transform.position;
+        vcam.SetActive(false);
+        vcam.SetActive(true);
     }
 }
---
+++ GoingPostal/Assets/Scripts/ExitLevel.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;using System.Collections.Generic;using UnityEngine;using UnityEngine.SceneManagement;public class ExitLevel : MonoBehaviour    {    public GameObject playerObj;
+    public GameObject startMenu;
+    public GameObject LevelMenu;    void Update()    {        if(Input.GetButtonDown("r") && playerObj){
+            SceneManager.LoadScene("Menu");
+            startMenu = GameObject.FindWithTag("StartMenu");
+            Debug.Log(startMenu);
+            startMenu.SetActive(false);
+        }    }    void OnTriggerEnter2D(Collider2D collider){        if(collider.CompareTag("Player")){            playerObj = collider.gameObject;        }    }    void OnTriggerExit2D(Collider2D collider)    {        playerObj = null;    }                    }
