3361c2dfeec84a0e0585bc1893080768126b53dc
“Nicholas <nporrone@uwo.ca>
2020-03-29 23:20:12 -0400

    Projectiles from player in Level 1

---
+++ GoingPostal/Assets/paperToss.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paperToss : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+}
