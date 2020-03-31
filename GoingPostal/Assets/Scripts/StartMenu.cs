1859f48e81d0ce49071e06043ac39efb4715c8dc
Riley Gabriele <rgabrie@uwo.ca>
2020-03-30 21:48:00 -0400

    Fixed some menu stuff

--- GoingPostal/Assets/Scripts/StartMenu.cs
+++ GoingPostal/Assets/Scripts/StartMenu.cs
@@ -8,9 +8,29 @@ public class StartMenu : MonoBehaviour
     // Start is called before the first frame update
     public void StartGame()
     {
+        // Level 2??
         SceneManager.LoadScene("Start");
+        Debug.Log("Level 2 loaded.");
     }
 
+    public void StartTutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+        Debug.Log("Tutorial level loaded.");
+    }
+
+    public void StartLevel1()
+    {
+        // Scene is missing in unity build settings
+        // SceneManager.LoadScene("Level 1");
+        // Debug.Log("Level 1 loaded.");
+    }
+
+    public void StartLevel3()
+    {
+        // SceneManager.LoadScene("Level 3");
+        // Debug.Log("Level 3 loaded.");
+    }
     public void QuitGame()
     {
         Debug.Log("QUIT!");
